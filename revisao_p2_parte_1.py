5bd4ce45e8fab9e2de053f69a03e290e20c7cf16
proffillipesilva <81185103+proffillipesilva@users.noreply.github.com>
2021-04-05 19:36:07 -0300

    Create revisao_p2_parte_1.py

---
+++ revisao_p2_parte_1.py
@@ -0,0 +1,13 @@
+# inicio
+def exercicio1():
+  nota1 = float(input("Digite a nota1: "))
+  nota2 = float(input("Digite a nota2: "))
+
+  soma = nota1 + nota2
+
+  if soma < 10:
+    print(f"Falhou com soma de notas igual a {soma}")
+  else:
+    print(f"Passou com soma de notas iguais a {soma}")
+
+# fim
