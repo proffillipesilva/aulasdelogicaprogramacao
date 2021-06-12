1b26da2436512cb418ea69efec45b929453e0df4
fillipesouza <ss.fillipe@gmail.com>
2021-06-12 12:00:45 -0300

    Corrigindo dependencia ciclica (#14)
    * Aula sobre classes e tipos
    
    * Corrigindo problema da importacao ciclica

--- aula1106/conta.py
+++ aula1106/conta.py
@@ -1,11 +1,18 @@
-from pessoa import Pessoa
+# O TypeChecking é usado para permitir tipos e evitar o problema da importacao ciclica
 
+from typing import TYPE_CHECKING, List
+if TYPE_CHECKING:
+    from aula1106.pessoa import Pessoa
+    from aula1106.movimentacoes import Deposito
 
 class Conta:
-    def __init__(self, id: int, pessoa: Pessoa):  # construtor
+
+    def __init__(self, id: int, pessoa: 'Pessoa'):  # construtor
         self._id = id
         self._pessoa = pessoa
         self._saldo = 0
+
+        self._depositos: List['Deposito'] = []
 
     def altera_saldo(self, valor: float):  #setter
         self._saldo += valor
@@ -16,5 +23,8 @@ class Conta:
     def get_saldo(self) -> float:  # getter
         return self._saldo
 
-    def get_pessoa(self) -> Pessoa:
+    def get_pessoa(self) -> 'Pessoa':
         return self._pessoa
+
+    def insere_deposito(self, deposito: 'Deposito'):
+        self._depositos.append(deposito)
--- aula1106/movimentacoes.py
+++ aula1106/movimentacoes.py
@@ -1,7 +1,11 @@
-from conta import Conta
+# O TypeChecking é usado para permitir tipos e evitar o problema da importacao ciclica
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from aula1106.conta import Conta
 
 class Deposito:
-    def __init__(self, remetente: Conta, destinatario: Conta, valor: float):
+
+    def __init__(self, remetente: 'Conta', destinatario: 'Conta', valor: float):
         self._remetente = remetente
         self._destinatario = destinatario
         self._valor = valor
