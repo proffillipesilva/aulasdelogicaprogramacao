67ef7b11e71c8b7f20a04eb942fff4de5fdac935
Fillipe Silva <f.silva@sidi.org.br>
2021-06-12 11:55:32 -0300

    Corrigindo problema da importacao ciclica

--- aula1106/main.py
+++ aula1106/main.py
@@ -1,22 +1,24 @@
-from conta import Conta
-from movimentacoes import Deposito
-from pessoa import Pessoa
-
+import conta
+import movimentacoes
+import pessoa
 if __name__ == '__main__':
-    naruto = Pessoa(cpf="15845221-01", nome="Naruto Usumaki")
-    kakashi = Pessoa(cpf="125442452-01", nome="Kakashi Sensei")
-    conta_kakashi = Conta(id=1234, pessoa=kakashi)
+
+    naruto = pessoa.Pessoa(cpf="15845221-01", nome="Naruto Usumaki")
+    kakashi = pessoa.Pessoa(cpf="125442452-01", nome="Kakashi Sensei")
+    conta_kakashi = conta.Conta(id=1234, pessoa=kakashi)
     print(conta_kakashi)
-    conta_naruto = Conta(id=3565, pessoa=naruto)
+    conta_naruto = conta.Conta(id=3565, pessoa=naruto)
     print(conta_naruto)
 
 
     print(conta_kakashi.get_saldo())
     print(conta_naruto.get_saldo())
 
-    deposito1 = Deposito(remetente=conta_naruto, destinatario=conta_kakashi, valor=36)
+    deposito1 = movimentacoes.Deposito(remetente=conta_naruto, destinatario=conta_kakashi, valor=36)
 
     print(conta_kakashi.get_saldo())
     print(conta_naruto.get_saldo())
+
+    conta_naruto.insere_deposito(deposito1)
 
 
